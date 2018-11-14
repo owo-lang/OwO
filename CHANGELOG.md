fa9df7ffed03e45250e19d8344369a3060f354a8
ice1000 <ice1000kotlin@foxmail.com>
2018-11-14 15:04:20 -0800

    [ cabal ] Init project

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for OwO
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ src/full/impossible.h
@@ -0,0 +1,6 @@
+#ifndef __IMPOSSIBLE_H__
+#define __IMPOSSIBLE_H__
+
+#define __IMPOSSIBLE__ (throwImpossible (Impossible __FILE__ __LINE__))
+
+#endif // __IMPOSSIBLE_H__
