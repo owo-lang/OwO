26a7bcd8d2054820bc6bf2ab97677860e54bcfc3
ice1000 <ice1000kotlin@foxmail.com>
2018-11-25 18:37:47 -0500

    [ #18 ] Perl very good! Improving display

--- src/test/testData/test_runner.pl
+++ src/test/testData/test_runner.pl
@@ -10,34 +10,38 @@ my @failure = ();
 my $success = 0;
 my $noTerm = scalar @ARGV && $ARGV[0] eq '--no-terminal';
 
-foreach my $fixture (map {substr $_, 0, -1} split(/[ \t\n]+/, `ls -G -d */`)) {
+sub ntr {return colored $_[0], 'green';}
+sub red {return colored $_[0], 'red';}
+sub redy {return colored $_[0], 'bold red';}
+
+foreach my $fixture (map {substr $_, 0, -1} split /[ \t\n]+/, `ls -G -d */`) {
     say "Fixture $fixture:";
     `touch $fixture.flags`;
     my $flags = `cat $fixture.flags`;
-    foreach my $case (split(/[ \t\n]+/, `ls -G $fixture/*.owo`)) {
+    foreach my $case (split /[ \t\n]+/, `ls -G $fixture/*.owo`) {
         say " Case $case:";
         my $out = $case =~ s/\.owo/\.out/rg;
         `touch $out`;
         my $diff = `owo $flags -c $case | diff - $out`;
         if (length $diff) {
             push @failure, $case;
-            map {say(colored("  $_", 'red'))} split(/\n/, $diff);
+            map {say red("  $_")} split /\n/, $diff;
             print colored("  Replace the golden value (y/N)? ", 'cyan');
             !$noTerm && getc eq 'y' ? `owo $flags -c $case > $out`
-                : say colored('  Leave it alone.', 'yellow');
+                : say colored('  Leave it alone.', 'bold yellow');
         } else {
-            say colored('  Passed!', 'green');
+            say ntr('  Passed!');
             $success++;
         }
     }
 }
 
 my $failed = scalar @failure;
-say 'Result: ', $failed ? colored('FAILED.', 'red') : colored('ok.', 'green'),
-    colored(" $success passed,", 'green'),
-    colored(" $failed failed.", $failed ? 'red' : 'white');
+say 'Result: ', $failed ? redy('FAILED.') : ntr('ok.'),
+    ntr(" $success passed,"),
+    colored(" $failed failed.", $failed ? 'bold red' : 'white');
 if ($failed) {
-    my $pretty = join("\n ", @failure);
-    say colored("Failing tests:\n $pretty", 'red');
+    my $pretty = join "\n ", @failure;
+    say red("Failing tests:\n $pretty");
     die;
 }
